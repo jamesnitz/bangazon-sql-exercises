d778a7ed9acf91d4b3f4350e43fcfe30dd0f1d03
jamesnitz <jamesanitz2@gmail.com>
2020-04-07 11:27:39 -0500

    first commit

---
+++ Bangazon-SQL-EXERCISES-QUERIES.sql
@@ -0,0 +1,6 @@
+﻿SELECT e.Id, e.FirstName, e.LastName, e.DepartmentId, d.[Name],
+CASE WHEN IsSupervisor = 1 THEN 'TRUE' ELSE 'FALSE' END as IsSupervisor
+FROM Employee e
+LEFT JOIN Department d
+ON DepartmentId = d.Id
+ORDER BY Name, LastName, FirstName
